7a0752e2766c2780e557f01ad7cb673696c2766e
aserwois <mateusz.gabriel90@gmail.com>
2020-01-18 17:53:37 +0100

    ArrayPushing

--- index.js
+++ index.js
@@ -1,74 +1,12 @@
-/*function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
-    var result = "";
-    result += "The " + myAdjective + " " + myNoun + " "+ myVerb + " " + myAdverb
-    return result;
+function nextInLine(arr, item) {
+    arr.push(item);
+    return arr.shift()
 }
 
-console.log(wordBlanks("dog","big", "run", "quickly"));
+var testArr = [1,2,3,4,5];
 
-//Array
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
 
-var ourArray = [["Mathew", 29], ["Pablo",22]];
-var mathewArray = ourArray[0];
-var myArray = ["Quincy", 1];
-
-console.log(ourArray[0][0]);
-
-//Push Array
-
-/*ourArray.push(["dog",12]);
-console.log(ourArray);
-
-var removedOurArray = ourArray.pop();
-console.log(ourArray);
-
-var removedFirstElem = ourArray.shift();
-console.log(ourArray);
-
-ourArray.unshift(['Happy',12]);
-console.log(ourArray);
-
-//Shopping list in array
-var myList = [["cereal", 3], ["milk", 2], ["bannas", 3], ["juice", 2], ["eggs", 12]];
-*/
-
-//functions
-function ourReusableFunction() {
-    console.log("Heyya, World");
-}
-
-ourReusableFunction();
-
-function reusableFunction() {
-    console.log("Hi World");
-}
-
-reusableFunction();
-
-function ourFunctionWithArgs(a, b) {
-    var result = a - b;
-    return result;
-}
-
-console.log(ourFunctionWithArgs(10,5));
-
-function funtionWithArg(a, b) {
-    var sum = a + b;
-    return sum;
-}
-
-console.log(funtionWithArg(2,2));
-
-
-var tabliczkaMnozenia;
-
-var outerWear = "T-Shirt";
-
-function myOutfit() {
-    var outerWear = "Jeans";
-    return outerWear;
-}
-
-console.log(myOutfit());
-console.log(outerWear);
 
