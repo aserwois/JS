b5de90dad60f5dafe68f6911d20fe34e5fe8f231
aserwois <mateusz.gabriel90@gmail.com>
2020-01-18 11:06:40 +0100

    objectBasic

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // Const
-const interestRate = 0.3;
+let interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
 
-// 
+// variable types
 let name = "Mathew";
 let age = 30;
 let isApproved = true;
 let firstName = undefined;
 let selectedColor = null;
+
+// Dynamic typing (We can change var type in every place in code)
+
+// Objects (Reference types: Objects, Arrays, Functions)
+
+let person = {
+    name: 'Mathew',
+    age: 30
+};
+
+person.name = 'Pedro';
+
+console.log(person)
+
