87942ce10d0c829fd4d6f46e0fc106ceb91c5f82
aserwois <mateusz.gabriel90@gmail.com>
2020-01-18 18:01:24 +0100

    testEqual

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-function nextInLine(arr, item) {
-    arr.push(item);
-    return arr.shift()
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true!";
+    }
+
+    return "No, it's false";
 }
 
-var testArr = [1,2,3,4,5];
+function trueOrFalse(wasThatTrue) {
+    if(wasThatTrue) {
+        return "Yes, that was true!";
+    }
 
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
+    return "No, false!!!";
+}
 
+console.log(trueOrFalse(true));
 
+//New
+
+function testEqual(val) {
+    if (val == 10) {
+        return "Equal :)";
+    }
+
+    return "Not equal :(";
+}
+
+console.log(testEqual(8));
+console.log(testEqual(10));
