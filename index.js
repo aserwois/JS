29a2fb7b7fdb86a6b8dccd9d66c8dc79269e5928
aserwois <mateusz.gabriel90@gmail.com>
2020-01-19 21:32:19 +0100

    arrayfunction

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var myPlants = [
     {
-        type: "flowers",
-        list: [
+        "type": "flowers",
+        "list": [
             "rose",
             "tulip",
             "dandelion"
         ]
     },
     {
-        type: "trees",
-        list: [
+        "type": "trees",
+        "list": [
             "fir",
             "pine",
             "applepie"
@@ -19,4 +19,19 @@ var myPlants = [
 
 var secondTree = myPlants[1].list[1];
 
-console.log(myPlants[1]);
+//console.log(myPlants[1]);
+
+function returnCollection(id, prop, value) {
+    if(value === "") {
+        delete myPlants[id][prop];
+    } else if ( prop === "list") {
+        myPlants[id][prop] = myPlants[id][prop] || [];
+        myPlants[id][prop].push(value);
+    } else {
+        myPlants[id][prop] = value;
+    }
+
+    return myPlants;
+}
+
+console.log(returnCollection(1,"list","głowa"));
