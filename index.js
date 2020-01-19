3cc4bace73a9fc7dac50c0831a24bc54460363cd
aserwois <mateusz.gabriel90@gmail.com>
2020-01-19 20:50:24 +0100

    objectinobject

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-function caseInSwitch(val) {
-    var answer = "";
-    switch(val) {
-        case 1:
-            answer = "alpha";
-            break;
-        case 2:
-            answer = "beta";
-            break;
-        case 3:
-            answer = "delta"
-            break;
-        case 4:
-            answer = "gamma"
-            break;
-        default:
-            answer = "There is no case like this one!";
-            break;
+var myStorage = {
+    car: {
+        inside: {
+            gloveBox: "maps",
+            passengerSeat: "crumbs"
+        },
+        outside: {
+            trunk: "jack",
+            dupa: "niewiem"
+        }
     }
-
-    return answer;
 }
 
-console.log(caseInSwitch(5));
+var myCarInside = myStorage.car.outside;
+
+console.log(myCarInside);
