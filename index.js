d054963747688df7a0a21fbf4b27faf2674e412a
aserwois <mateusz.gabriel90@gmail.com>
2020-01-19 19:13:46 +0100

    switch

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var names = ["Hole","Eagle","Birdie","Par","Bogey","Double Bogey"];
-
-function golfScore (par, strokes) {
-    if (strokes == 1) {
-        return names[0];
-    } else if (strokes <= par - 2) {
-        return name[1];
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "delta"
+            break;
+        case 4:
+            answer = "gamma"
+            break;
+        default:
+            answer = "There is no case like this one!";
+            break;
     }
 
-    return "Change Me";
+    return answer;
 }
 
-golfScore(5, 4)
+console.log(caseInSwitch(5));
