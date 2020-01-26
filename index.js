12194fa11544b416c353f72679bb0015e1b15201
aserwois <mateusz.gabriel90@gmail.com>
2020-01-26 20:46:42 +0100

    JSCwiczenie

--- index.js
+++ index.js
@@ -16,3 +16,35 @@ for (var i = 0; i < 5; i++) {
 }
 
 console.log(ourArray);
+
+const mouseOver = (event) => {
+    console.log('mouseover', event)
+    event.target.style.color = 'white'
+    event.target.style.top += '50px'
+    event.target.style.left += '50px'
+}
+
+setColor = function(dick) {
+    document.body.style.backgroundColor = dick.target.value
+    if(dick.target.value == 'green') {
+        document.getElementById('zielono').style.display = "block";
+    } if(dick.target.value == 'red') {
+        document.getElementById('czerwono').style.display = "block";
+    } if(dick.target.value == 'blue') {
+        document.getElementById('niebiesko').style.display = "block";
+    } if(dick.target.value == 'yellow') {
+        document.getElementById('zolto').style.display = "block";
+    } 
+
+}
+
+displayText = function(dickenson) {
+    event.target.style.visibility = 'visible'
+}
+
+
+
+// select z 4 opcjami green red blue yellow, defaultowo ma byc green
+// wysrodkowac horyzontalnie i verticalnie *
+// onChange zmienic background strony
+// document.getElement .... 
