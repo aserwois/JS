3e5bc91e5225dc16e91123773ade2a2a34b6cd98
aserwois <mateusz.gabriel90@gmail.com>
2020-02-03 18:23:56 +0100

    PrzedstawSieFunkcjaJS

---
+++ jsa.js
@@ -0,0 +1,10 @@
+
+function startGame() {
+var firstName = prompt("Jak masz na imię?")
+var lastName = prompt("Jak masz na nazwisko?")
+var age = prompt("Ile massz lat?")
+
+var fullName = firstName + " " + lastName
+
+alert("Witaj " + fullName + ", wszyscy wiedzą, że masz " + age + " lat")
+}
