8ef134a4a0dddf1cb3117a873d3eb0160b33b83e
aserwois <mateusz.gabriel90@gmail.com>
2020-02-03 19:10:12 +0100

    ageFunction

--- jsa.js
+++ jsa.js
@@ -8,3 +8,15 @@ var fullName = firstName + " " + lastName
 
 alert("Witaj " + fullName + ", wszyscy wiedzą, że masz " + age + " lat")
 }
+
+function ageGame() {
+    var wiek = prompt("Ile masz lat?")
+
+    var dni = wiek * 365.2
+
+    if(wiek <= 0 || wiek.length <= 0) {
+        alert("Jeszcze sie nie urodziłeś :D babasku:)")
+    } else {
+    alert("Przeżyłeś już " + dni + " dni mistrzu! Tak trzymaj :)!")
+    }
+}
