d6c4877304f44daed12ada347dab301eb8f2eaea
aserwois <mateusz.gabriel90@gmail.com>
2020-02-06 20:18:08 +0100

    firstArray

---
+++ jsaArray.js
@@ -0,0 +1,20 @@
+var friend = ["Mathew","Łozi","Karol"]
+
+// alert(friend[0]);
+
+var friends = [];
+var friends = new Array();
+
+// array has length/can hold any type of data
+
+friend.push("Ktośtam");
+friend.push("cośtam");
+
+// unshift - dodac na poczatek tablicy
+// shift - usunac pierwszy item z tablicy
+
+indexOf
+
+friend.indexOf("Mathew");
+
+friend.indexOf("dupa") zwraca -1 jesli nie ma elementu
