c6f208d300eef802b5108d40ce1e8b1f45e93647
aserwois <mateusz.gabriel90@gmail.com>
2020-02-07 15:58:26 +0100

    refactoring

--- jsaArray.js
+++ jsaArray.js
@@ -3,17 +3,33 @@ var todos = [];
 window.setInterval(function() {
     var firstQuestion = prompt("What you would like to do?")
     if(firstQuestion === "list") {
-        console.log(todos);
+        listTodos();
+    } else if (firstQuestion === "new") {
+        addTodo();
+    }  else if (firstQuestion === "quit") {
+       quitTodo();
+    } else if (firstQuestion = "delete") {
+        deleteTodo();
+    }
+}, 500)
+
+function listTodos() {
+    console.log(todos);
         todos.forEach(function(item, i) {
             console.log(i + ": " + item);
         })
-    } else if (firstQuestion === "new") {
-        var addNewItem = prompt("Please write new list item");
+}
+
+function addTodo() {
+    var addNewItem = prompt("Please write new list item");
         todos.push(addNewItem);
-    }  else if (firstQuestion === "quit") {
-       return alert("Thanks for exercising!");
-    } else if (firstQuestion = "delete") {
-        var indexToDelete = prompt("Write an index which you want to delete:)")
+}
+
+function deleteTodo() {
+    var indexToDelete = prompt("Write an index which you want to delete:)")
         todos.splice(indexToDelete, 1);
-    }
-}, 500)
+}
+
+function quitTodo() {
+    return alert("Thanks for exercising!");
+}
