36a6b13beba120a2ee74b1741351e6eeac103a6e
aserwois <mateusz.gabriel90@gmail.com>
2020-02-07 20:16:31 +0100

    addFunctionToObject

--- object.js
+++ object.js
@@ -1,24 +1,38 @@
-var movies = [
-    {
-        title: "Yes Man",
-        watched: true,
-        rating: 4.5
-    },
-    {
-        title: "Wojna",
-        watched: false,
-        rating: 1.5
-    },
-    {
-        title: "No man",
-        watched: false,
-        rating: 5
-    }
-]
+// var movies = [
+//     {
+//         title: "Yes Man",
+//         watched: true,
+//         rating: 4.5
+//     },
+//     {
+//         title: "Wojna",
+//         watched: false,
+//         rating: 1.5
+//     },
+//     {
+//         title: "No man",
+//         watched: false,
+//         rating: 5
+//     }
+// ]
 
-movies.forEach(function(movie) {
-    if(movie.watched) {
-        console.log("I have seen that movie called " + movie.title + ". Rating of that is " + movie.rating + " stars.");
+// movies.forEach(function(movie) {
+//     if(movie.watched) {
+//         console.log("I have seen that movie called " + movie.title + ". Rating of that is " + movie.rating + " stars.");
+//     }
+//     console.log("I have not seen that movie called " + movie.title + ". Rating of that is " + movie.rating + " stars.");
+// })
+
+var obj = {
+    name: "Chuck",
+    age: 45,
+    isCool: true,
+    friends: ["Bogdan","Mati"],
+    add: function(x, y) {
+        return x + y;
     }
-    console.log("I have not seen that movie called " + movie.title + ". Rating of that is " + movie.rating + " stars.");
-})
+}
+
+obj.maybe = function() {
+    return "I dont know!"
+}
