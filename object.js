8d627de39168e7692abe99a6427d987afc08059a
aserwois <mateusz.gabriel90@gmail.com>
2020-02-07 19:37:36 +0100

    firstObject

---
+++ object.js
@@ -0,0 +1,28 @@
+// var person = {
+//     name: "Mathew",
+//     age: 29,
+//     city: "Warsaw"
+// };
+
+// console.log(person.name);
+// console.log(person["age"])
+
+// var cat = {
+//     name: "Batman",
+//     age: 1,
+//     city: "Warsaw"
+// }
+
+// console.log(cat.city) 
+
+var someObject = {
+    friends: [
+        {name: "Malfoy"},
+        {name: "Crabbe"},
+        {name: "Goyle"}
+    ],
+    color: "baby blue",
+    isEvil: true
+}
+
+someObject.friends[0].name;
