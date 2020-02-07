fbee90fb015e7f0bf96fbdf457d1f49cbf85a4ab
aserwois <mateusz.gabriel90@gmail.com>
2020-02-07 15:54:22 +0100

    addDeleteButton

--- jsaArray.js
+++ jsaArray.js
@@ -4,10 +4,16 @@ window.setInterval(function() {
     var firstQuestion = prompt("What you would like to do?")
     if(firstQuestion === "list") {
         console.log(todos);
+        todos.forEach(function(item, i) {
+            console.log(i + ": " + item);
+        })
     } else if (firstQuestion === "new") {
         var addNewItem = prompt("Please write new list item");
         todos.push(addNewItem);
     }  else if (firstQuestion === "quit") {
        return alert("Thanks for exercising!");
+    } else if (firstQuestion = "delete") {
+        var indexToDelete = prompt("Write an index which you want to delete:)")
+        todos.splice(indexToDelete, 1);
     }
 }, 500)
