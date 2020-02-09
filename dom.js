96e2b411a39847246d7175d0d8346915a341db32
aserwois <mateusz.gabriel90@gmail.com>
2020-02-09 11:14:45 +0100

    QuerySelector

--- dom.js
+++ dom.js
@@ -1,18 +1,26 @@
+// var h1 = document.querySelector("h1");
+
+// h1.style.color = "white";
+
+// var body = document.querySelector("body");
+// var isPink = false;
+
+// setInterval(function() {
+//     if(isPink) {
+//         body.style.backgroundColor = "white";
+//         h1.style.color = "Pink";
+//     } else {
+//         body.style.backgroundColor = "Pink";
+//         h1.style.color = "White";
+//     }
+
+//     isPink = !isPink;
+// }, 1000);
+// var utag = document.querySelector(".dupcia");
+// var ptag = document.querySelector("p");
+// var itag = document.getElementById("first");
+// var ctag = document.getElementsByClassName("dupcia");
+
 var h1 = document.querySelector("h1");
 
-h1.style.color = "white";
-
-var body = document.querySelector("body");
-var isPink = false;
-
-setInterval(function() {
-    if(isPink) {
-        body.style.backgroundColor = "white";
-        h1.style.color = "Pink";
-    } else {
-        body.style.backgroundColor = "Pink";
-        h1.style.color = "White";
-    }
-
-    isPink = !isPink;
-}, 1000);
+h1.classList.add(".h1Class")
