36b705ec843385cc95c3948f13ed4e73d26cc4b4
aserwois <mateusz.gabriel90@gmail.com>
2020-02-19 23:05:15 +0100

    forEach

--- forOfRestSpreadJs.js
+++ forOfRestSpreadJs.js
@@ -1,13 +1,51 @@
-var arr = [1,2,3,4,5]
+// var arr = [1,2,3,4,5]
 
-// for(let val of arr){
+// arr.forEach(function(val,i,arr) {
 //     console.log(val);
-// }
+// })
 
-function printRest(a,b,...c) {
-    console.log(a),
-    console.log(b),
-    console.log(c)
+function pushToArr(arra) {
+    var newArra = [];
+    arra.forEach(function(val) {
+        newArra.push(val / 2);
+    })
+
+    console.log(newArra);
 }
 
-printRest(1,2,3,4,5);
+pushToArr([2,4,6])
+
+// // for(let val of arr){
+// //     console.log(val);
+// // }
+
+// function printRest(a,b,...c) {
+//     console.log(a),
+//     console.log(b),
+//     console.log(c)
+// }
+
+// printRest(1,2,3,4,5);
+
+// Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
+
+// Examples:
+//     doubleValues([1,2,3]) // [2,4,6]
+//     doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
+
+// */
+function doubleValues(arr){
+    var newArr = [];
+    arr.forEach(function(val) {
+        newArr.push(val * 2);
+    });
+    return newArr;
+}
+
+function onlyEvenValues(arr){
+    var newArr = [];
+    arr.forEach(function(val) {
+        newArr.push(val % 2 === 0);
+    });
+    return newArr;
+}
