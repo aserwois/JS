39769a2f95b248367c8414071ff8770d1db28366
aserwois <mateusz.gabriel90@gmail.com>
2020-02-23 21:46:01 +0100

    strictModeFunctionThis

--- closuresThis.js
+++ closuresThis.js
@@ -3,3 +3,21 @@
 var inst = "a"
 
 window.inst === inst;
+
+function whatIsThis(){
+    return this;
+}
+
+whatIsThis(); //window
+
+function varablesInThis(){
+    this.person = "Elie";
+    var idiota = "d"
+}
+
+varablesInThis()
+console.log(person)
+console.log(idiota)
+
+//strict mode "use strict" is preventing us from this
+//using Typeerror.
