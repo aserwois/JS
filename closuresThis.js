8f85e3b80cc9446ca6027e2e6df61faae93c972f
aserwois <mateusz.gabriel90@gmail.com>
2020-02-24 22:40:12 +0100

    closure2

--- closuresThis.js
+++ closuresThis.js
@@ -8,3 +8,26 @@ function specialMultiply(a, b) {
 }
 
 specialMultiply(3)(4); // 12
+
+function guessingGame(amount) {
+    var answer = Math.floor(Math.random() * 11),
+        guesses = 0;
+
+    return function (guess) {
+
+        //Increment guesses
+        guesses++;
+
+        //Return if guess limit exceeded
+        if (guesses > amount) return 'No more guesses the answer was ' + answer;
+
+        //Check if guess is high, low or correct, return message
+        if (guess > answer) {
+            return 'Your guess is too high!';
+        }
+        else if (guess < answer) { 
+            return 'Your guess is too low!';
+        }
+        else { 
+            return "cos tam";
+        }
