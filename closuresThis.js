daa7c628a2a8efb74204b4138b941e9c1a56c412
aserwois <mateusz.gabriel90@gmail.com>
2020-02-25 17:20:30 +0100

    Spread

--- closuresThis.js
+++ closuresThis.js
@@ -1,35 +1,38 @@
-// always return an array
-// is called REST only when it is parameter
-// in a function
-// is accessed without ... in function
+// Spread
+// used on array to spread each value(as comma seperated val)
+// for array when expects comma separated values
 
-function printRest(a,b,...c) {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
+//ES5
+var arr1 = [1,2,3];
+var arr2 = [4,5,6];
+var arr3 = [7,8,9];
 
-printRest(1,2,3,4,5);
-
-// 1
-// 2
-// [3,4,5]
-
-// ES5
-function sumArguments() {
-  var total = 0
-  for(i = 0; i < arguments.length; i++) {
-    total += arguments[i];
-  }
-  return total;
-}
-
-function sumArguments() {
-  var argumentsArray = [].slice.call(arguments);
-  return argumentsArray.reduce(function(acc,next) {
-    return acc + next;
-  });
-}
+var combined = arr1.concat(arr2).concat(arr3);
 
 //ES2015
-var sumArguments = (...args) => args.reduce((acc,next) => acc + next);
+
+var combined = [...arr1, ...arr2, ...arr3];
+
+var arr = [1,2,3,4,5];
+
+Math.max(arr); // NaN
+
+//ES5
+
+Math.max.apply(this, arr); // 5 
+
+//ES2015
+
+Math.max(...arr); // 5
+
+function sumValues(a,b,c) {
+  return a+b+c
+}
+
+var nums = [12,15,20];
+
+sumValues(...nums); // 47
+
+var sumValues = (...args) => args.reduce((acc, next) => acc + next)
+
+sumValues.apply(this, nums);
