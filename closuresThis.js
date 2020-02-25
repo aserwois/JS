aa4ed8787c60b30c52e1b56b09d81a7cd92cc237
aserwois <mateusz.gabriel90@gmail.com>
2020-02-25 18:35:12 +0100

    ObjectDestructuringDefaultValues

--- closuresThis.js
+++ closuresThis.js
@@ -1,38 +1,93 @@
-// Spread
-// used on array to spread each value(as comma seperated val)
-// for array when expects comma separated values
+var firstName = "Mateusz";
+var lastName = "Gabriel";
 
-//ES5
-var arr1 = [1,2,3];
-var arr2 = [4,5,6];
-var arr3 = [7,8,9];
-
-var combined = arr1.concat(arr2).concat(arr3);
-
-//ES2015
-
-var combined = [...arr1, ...arr2, ...arr3];
-
-var arr = [1,2,3,4,5];
-
-Math.max(arr); // NaN
-
-//ES5
-
-Math.max.apply(this, arr); // 5 
-
-//ES2015
-
-Math.max(...arr); // 5
-
-function sumValues(a,b,c) {
-  return a+b+c
+var instructor = {
+  firstName: firstName,
+  lastName: lastName
 }
 
-var nums = [12,15,20];
+// ES2015
 
-sumValues(...nums); // 47
+var instructor = {
+  firstName,
+  lastName
+}
 
-var sumValues = (...args) => args.reduce((acc, next) => acc + next)
+// ES5
 
-sumValues.apply(this, nums);
+var instructor = {
+  sayHello: function() {
+    return `Hello!`;
+  }
+}
+
+// ES2015 - dont use arrow because they dont have their own this keyword
+
+var instructor = {
+  sayHello() {
+    return `Hello!`;
+  }
+}
+
+// ES5
+var firstNames = "Mati";
+var instructior = {};
+instructior[firstNames] = "That's me!";
+
+instructior.firstNames;
+
+//ES2015
+
+var firstName = "Mati"
+var instructor = {
+  [firstName]: "That's me!";
+}
+
+instructor.Mati;  // That's me
+
+// Object destructuring!
+
+var instructor = {
+  firstName = "Mateusz";
+  lastName = "Gabriel";
+}
+
+var firstName = instuctor.firstName;
+var lastName = instructor.lastName;
+
+//ES2015
+
+var {firstName, lastName} = instructor;
+
+var {firstName:first, lastName:last} = instructor;
+
+first;
+last;
+
+function createInstructor(options) {
+  var options = options || {};
+  var name = options.name || {firstName: "Mateusz", lastName: "Gabriel"};
+  var isHilarious = options.isHilarious || false;
+  return [name.firstName, name.lastName, isHilarious]
+}
+
+createInstructor();
+createInstructor({isHilarious:true});
+createInstructor({name: {firstName: "Mati", lastName: "Kowalski"}})
+
+// ES2015 - default values ponizej a jak sie nie uda to pusty obiekt tworzymy
+
+function createInstructor({name = {first: "Mati", last: "Gabriel"}, isHilarious = false} = ()){
+  return [name.first, name.last, isHilarious]
+}
+
+function displayObj({name,color}) {
+  return [name, color];
+}
+
+var instructor = {
+  name: "Mateusz";
+  color: "blue"
+};
+
+displayInfo(instructor); // ["Mateusz","blue"]
