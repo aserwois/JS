7812cf55bc557b5fccedea2b23c0394e1dc3198b
aserwois <mateusz.gabriel90@gmail.com>
2020-02-25 16:57:14 +0100

    RestReduceArrow

--- closuresThis.js
+++ closuresThis.js
@@ -1,24 +1,35 @@
-//Default Parameters
+// always return an array
+// is called REST only when it is parameter
+// in a function
+// is accessed without ... in function
 
-function add(a=10,b=20) {
-  return a + b;
+function printRest(a,b,...c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
 }
 
-add(); // 30
+printRest(1,2,3,4,5);
 
-add(20) // 40
+// 1
+// 2
+// [3,4,5]
 
-add(20,30) // 50
+// ES5
+function sumArguments() {
+  var total = 0
+  for(i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+}
 
-//For .. of Loops
+function sumArguments() {
+  var argumentsArray = [].slice.call(arguments);
+  return argumentsArray.reduce(function(acc,next) {
+    return acc + next;
+  });
+}
 
-var arr = [1,2,3,4,5];
-
-for(let val of arr) {
-  console.log(val);
-}  // 1 2 3 4 5
-
-// -Can't access an index, -Not working on objects
-
-
-
+//ES2015
+var sumArguments = (...args) => args.reduce((acc,next) => acc + next);
