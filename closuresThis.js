0eb4bf21cf9b62b3a20f6a4a0b97e9be09492ffd
aserwois <mateusz.gabriel90@gmail.com>
2020-02-25 12:27:13 +0100

    constructosFunctionsExercises

--- closuresThis.js
+++ closuresThis.js
@@ -1,54 +1,63 @@
-function Person(name) {
-    this.name = name;
-    this.sayHi = function() {
-        return `Hi ${this.name}`
-    }
+function Student(firstname,lastname) {
+    return person.apply(this, arguments)
 }
 
-elie = new Person("Elie");
-elie.sayHi();
+Student.prototype = Person.prototype;
 
-function Person(name) {
-    this.name = name;
-}
+// inheritance of all methods both ways
 
-Person.prototype.sayHi = function() {
-    return `Hi ${this.name}`;
-}
+Student.prototype = Object.create(Person.prototype)
 
-elie = new Person("Elie");
-elie.sayHi();
+Student.prototype.constructor // Person
+
+Student.prototype.constructor = Student;
+
+// 1 - Create a constructor function for a Vehicle. Each vehicle should have a make, model and year property.
 
 function Vehicle(make, model, year) {
     this.make = make;
     this.model = model;
     this.year = year;
-    this.isRunning = false;
-};
-
-Vehicle.prototype.turnOn = function() {
-    return this.isRunning = true;
-}
-
-Vehicle.prototype.turnOff = function() {
-    return this.isRunning = false;
-}
-
-Vehicle.prototype.beep = function() {
-    if(this.isRunning) {
-        return `Beeep!`
-    }
-    return `Your car is not running, sorry!`
-}
-
-var opel = new Vehicle("Opel","Astra",1990)
-
-opel.isRunning();
-
-Person.prototype.addToFamily = function(personObj) {
-    if(personObj instanceof Person && this.family.indexOf(personObj) === -1) {
-      this.family.push(personObj);
-    }
-    
-    return this.family.length;
-  };
+  }
+  
+  // 2 - Add a function to the Vehicle prototype called start which returns the string "VROOM!"
+  
+  Vehicle.prototype.start = function () {
+    return "VROOM!";
+  }
+  
+  // 3 - Add a function to the Vehicle prototype called toString which returns the string "The make, model, and year are" concatenated with the make, model and year property
+  
+  Vehicle.prototype.toString = function() {
+    return 'The make, model, and year are ' + this.make + ' ' + this.model + ' ' + this.year;
+  }
+  
+  /* Examples 
+      var vehicle = new Vehicle("Tractor", "John Deere", 1999)
+      vehicle.toString() // 'The make, model, and year are Tractor John Deere 1999'
+  */
+  
+  // 4 - Create a constructor function for a Car. Each object created from the Car function should also have a make, model, and year and a property called numWheels which should be 4. The Car prototype should inherit all of the methods from the Vehicle prototype
+  
+  function Car(make, model, year) {
+    Vehicle.apply(this, arguments);
+    this.numWheels = 4;
+  }
+  
+  Car.prototype = Object.create(Vehicle.prototype);
+  Car.prototype.constructor = Car;
+  
+  // 5 - Create a constructor function for a Motorcycle. Each object created from the Motorcycle function should also have a make, model, and year and a property called numWheels which should be 2. The Motorcycle prototype should inherit all of the methods from the Vehicle prototype
+  
+  function Motorcycle(make, model, year) {
+    Vehicle.apply(this, arguments);
+    this.numWheels = 2;
+  }
+  
+  Motorcycle.prototype = Object.create(Vehicle.prototype);
+  Motorcycle.prototype.constructor = Motorcycle;
+  
+  
+  //TESTING
+  var myCar = new Car('someMake', 'someModel', 2010);
+  myCar.start()
