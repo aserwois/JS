a8b8ca6753e548a7cd4c3fe5d0c01e7175eafbbb
aserwois <mateusz.gabriel90@gmail.com>
2020-02-29 19:26:07 +0100

    mapslikeObjects

--- closuresThis.js
+++ closuresThis.js
@@ -1,93 +1,29 @@
-var firstName = "Mateusz";
-var lastName = "Gabriel";
-
-var instructor = {
-  firstName: firstName,
-  lastName: lastName
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastname;
 }
 
-// ES2015
-
-var instructor = {
-  firstName,
-  lastName
+Person.prototype.sayHello() {
+  return `Hello ${this.firstName} ${this.lastName}`
 }
 
-// ES5
+function Student() {
+  Person.call(this, arguments);
+}
 
-var instructor = {
-  sayHello: function() {
-    return `Hello!`;
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
-}
-
-// ES2015 - dont use arrow because they dont have their own this keyword
-
-var instructor = {
   sayHello() {
-    return `Hello!`;
+    return `Hello ${this.firstName} ${this.lastName}`
   }
 }
 
-// ES5
-var firstNames = "Mati";
-var instructior = {};
-instructior[firstNames] = "That's me!";
-
-instructior.firstNames;
-
-//ES2015
-
-var firstName = "Mati"
-var instructor = {
-  [firstName]: "That's me!";
+class Student extends Person {
+  constructor(firstName, lastName) {
+    super(firstName, lastName);
+  }
 }
 
-instructor.Mati;  // That's me
-
-// Object destructuring!
-
-var instructor = {
-  firstName = "Mateusz";
-  lastName = "Gabriel";
-}
-
-var firstName = instuctor.firstName;
-var lastName = instructor.lastName;
-
-//ES2015
-
-var {firstName, lastName} = instructor;
-
-var {firstName:first, lastName:last} = instructor;
-
-first;
-last;
-
-function createInstructor(options) {
-  var options = options || {};
-  var name = options.name || {firstName: "Mateusz", lastName: "Gabriel"};
-  var isHilarious = options.isHilarious || false;
-  return [name.firstName, name.lastName, isHilarious]
-}
-
-createInstructor();
-createInstructor({isHilarious:true});
-createInstructor({name: {firstName: "Mati", lastName: "Kowalski"}})
-
-// ES2015 - default values ponizej a jak sie nie uda to pusty obiekt tworzymy
-
-function createInstructor({name = {first: "Mati", last: "Gabriel"}, isHilarious = false} = ()){
-  return [name.first, name.last, isHilarious]
-}
-
-function displayObj({name,color}) {
-  return [name, color];
-}
-
-var instructor = {
-  name: "Mateusz";
-  color: "blue"
-};
-
-displayInfo(instructor); // ["Mateusz","blue"]
