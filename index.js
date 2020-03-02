41b32bbe29480d3cce2e8a09957cbb10d26e59c1
aserwois <mateusz.gabriel90@gmail.com>
2020-03-02 13:09:48 +0100

    ReactJsJsx

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
-var myPlants = [
-    {
-        "type": "flowers",
-        "list": [
-            "rose",
-            "tulip",
-            "dandelion"
-        ]
-    },
-    {
-        "type": "trees",
-        "list": [
-            "fir",
-            "pine",
-            "applepie"
-        ]
-    }
-]
+const renderSth = 3.323131;
 
-var secondTree = myPlants[1].list[1];
+const stringRenderowy = `1 dolar jest wart ${renderSth.toFixed(2)} dolarów`
 
-//console.log(myPlants[1]);
-
-function returnCollection(id, prop, value) {
-    if(value === "") {
-        delete myPlants[id][prop];
-    } else if ( prop === "list") {
-        myPlants[id][prop] = myPlants[id][prop] || [];
-        myPlants[id][prop].push(value);
-    } else {
-        myPlants[id][prop] = value;
-    }
-
-    return myPlants;
-}
-
-console.log(returnCollection(1,"list","głowa"));
+reactDOM.render(stringRenderowy, document.getElementById("app"))
