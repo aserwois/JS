0b683752c26b2d3124b54f09c36bccec15c2d621
aserwois <mateusz.gabriel90@gmail.com>
2020-03-10 12:12:11 +0100

    parentChildStateAsProp

--- React/testing-app/src/App.js
+++ React/testing-app/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import PropTest from './propTest'
+import PropTypes from 'prop-types'
 import './App.css';
 import FunctionProps from './functionprops'
+
+const InstructorItem = props => {
+    return (
+      <li>
+        <h3>{props.name}</h3>
+        <h4>Hobbies: {props.hobbies.join(" ,")}</h4>
+      </li>
+    );
+  }
+
+InstructorItem.propTypes = {
+  name: PropTypes.string,
+  hobbies: PropTypes.arrayOf(PropTypes.string)
+}
 
 class App extends Component {
   constructor(props) {
@@ -35,24 +50,20 @@ class App extends Component {
           this.state.instructors[randInst].hobbies.length
           );
 
-        const dupa = this.state.instructors.map((inst, i) => (
-          i === randInst ? {
-
-          } : inst
-    ))
-
+        const dupa = this.state.instructors
         // instructors[randInst] = Object.assign({}, instructors[randInst])
         // instructors[randInst].hobbies = instructors[randInst].hobbies.slice();
-        // dupa[randInst].hobbies.splice(hobbyIndex, 1, "dupeczka")
-        // this.setState({dupa});
+        dupa[randInst].hobbies.splice(hobbyIndex, 1, "dupeczka")
+        this.setState({dupa});
       }, 5000)
   }
+
   render () {
     const instructors = this.state.instructors.map((instr, i) => (
-      <li key={i}>
-        <h3>{instr.name}</h3>
-        <h4>Hobbies: {instr.hobbies.join(" ,")}</h4>
-      </li>
+      <InstructorItem key={i}
+        name={instr.name}
+        hobbies={instr.hobbies}
+      />
     ))
     return (
       <div className="App">
